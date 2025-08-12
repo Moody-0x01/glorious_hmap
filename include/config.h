aa2fc1cf25091a07cc7f9e9bdfe007ce465642d6
Lahoucine Azmoud <lazmoud@e2r6p6.1337.ma>
2025-08-12 09:38:43 +0100

    I gave up on making another impl, because linear probing and open addressing are the same thing.. likely have the same performance and i will be able to optimize open addressing to suit my neeeds..

--- include/config.h
+++ include/config.h
@@ -1,5 +1,9 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 // #define HMAP_LLIMPLEMENTATION
+/*
+ * This hashtable implementation defaults to open addressing
+ * if you need to use seperate chaining u probably gotta uncomment line 8
+ * */
 #define HMAP_DYN_ARRAY
 #endif // !CONFIG_H
